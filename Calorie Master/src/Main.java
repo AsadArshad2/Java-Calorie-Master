9dc593443dcfe659f2bcb645fa14c6bcf6895f0f
AsadArshad2 <asadarshad456@gmail.com>
2025-03-18 12:54:58 +0000

    added BMR return

--- Calorie Master/src/Main.java
+++ Calorie Master/src/Main.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
@@ -7,10 +8,14 @@ public class Main {
         final int weight; //KG
         final int height; //CM
         final char gender; //M or F
+        double bmr;
+        int bmrRounded;
+
         Scanner scanner = new Scanner(System.in);
         // Plan
         // 1 Ask for Age, Gender, Weight, Height
         // 2 Return BMR
+        //Men: BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) – (5.677 x age in years)
         // 3 Ask for activity level
         // 4 Return Rough TDEE
         // 5 Ask if person would like to lose or gain weight
@@ -22,9 +27,23 @@ public class Main {
         System.out.println("What is your height?");
         height = scanner.nextInt();
         System.out.println("What is your gender? Please enter the letter M or F");
+//        while(scanner.hasNext()) {
+//            System.out.println("What is your gender? Please enter the letter M or F");
+//        }
         gender = scanner.next().charAt(0);
 
-        System.out.println(age + " " + weight + " " + height + " " + gender);
+
+        if (gender == 'M' || gender == 'm') {
+            bmr = 88.362 + (13.397*weight) + (4.799 * height) +  - (6.677 * age);
+            bmrRounded = (int) bmr;
+            System.out.println("You have a BMR of " + bmrRounded);
+        }
+
+        if (gender == 'F' || gender == 'f') {
+            bmr = 447.593 + (9.247*weight) + (3.098*height) - (4.330*age);
+            bmrRounded = (int) bmr;
+            System.out.println("You have a BMR of " + bmrRounded);
+        }
 
 
 
@@ -33,7 +52,10 @@ public class Main {
 
 
 
-        System.out.println("Hello world!");
+
+
+
+        System.out.println("end");
     }
 
 }
